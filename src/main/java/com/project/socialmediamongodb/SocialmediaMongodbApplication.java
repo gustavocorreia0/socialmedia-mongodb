747a0d3f61a56165e7f1977e2fb2f6ef72bc749c
Gustavo Correia <gustavoCorreiaa0@gmail.com>
2024-03-03 09:35:16 -0300

    Project created

---
+++ src/main/java/com/project/socialmediamongodb/SocialmediaMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.project.socialmediamongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialmediaMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialmediaMongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/socialmediamongodb/SocialmediaMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.socialmediamongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialmediaMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
