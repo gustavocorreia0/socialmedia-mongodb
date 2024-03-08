c8df178acdd1732532a9362e36a02b9e8e8773f4
Gustavo Correia <gustavoCorreiaa0@gmail.com>
2024-03-07 23:40:38 -0300

    User DTO created

--- src/main/java/com/project/socialmediamongodb/resources/UserResource.java
+++ src/main/java/com/project/socialmediamongodb/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.project.socialmediamongodb.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.project.socialmediamongodb.domain.User;
+import com.project.socialmediamongodb.dto.UserDTO;
 import com.project.socialmediamongodb.services.UserService;
 
 @RestController
@@ -19,8 +21,10 @@ public class UserResource {
 	private UserService userService;
 	
 	@GetMapping
-	public ResponseEntity<List<User>> findAll(){
-		return ResponseEntity.ok().body(userService.findAll());
+	public ResponseEntity<List<UserDTO>> findAll(){
+		List<User> list = userService.findAll();
+		List<UserDTO> listDTO = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
 	}
 
 }
