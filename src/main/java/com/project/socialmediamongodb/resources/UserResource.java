6a79cea11366bc7a1e2a05b4444f8d3c4a85ac31
Gustavo Correia <gustavoCorreiaa0@gmail.com>
2024-03-10 22:59:38 -0300

    Uptade, delete and register User

--- src/main/java/com/project/socialmediamongodb/resources/UserResource.java
+++ src/main/java/com/project/socialmediamongodb/resources/UserResource.java
@@ -1,14 +1,20 @@
 package com.project.socialmediamongodb.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.project.socialmediamongodb.domain.User;
 import com.project.socialmediamongodb.dto.UserDTO;
@@ -32,4 +38,26 @@ public class UserResource {
 	public ResponseEntity<UserDTO> findById(@PathVariable String id){
 		return ResponseEntity.ok().body(new UserDTO(userService.findById(id)));
 	}
+	
+	@PostMapping
+	public ResponseEntity<Void> registerUser(@RequestBody UserDTO obj){
+		User newUser = userService.insert(userService.fromDTO(obj));
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(newUser.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> deleteUser(@PathVariable String id){
+		userService.delete(id);;
+		return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<User> updateUser(@PathVariable String id, @RequestBody UserDTO userData){
+		User user = userService.fromDTO(userData); 
+		user.setId(id);
+		user = userService.update(user);
+		return ResponseEntity.ok().body(user);
+	}
+	
 }
--- src/main/java/com/project/socialmediamongodb/services/UserService.java
+++ src/main/java/com/project/socialmediamongodb/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.project.socialmediamongodb.domain.User;
+import com.project.socialmediamongodb.dto.UserDTO;
 import com.project.socialmediamongodb.repositories.UserRepository;
 import com.project.socialmediamongodb.services.exception.ObjectNotFoundException;
 
@@ -23,7 +24,31 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = userRepository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
-
 	}
-
+	
+	public User insert(User newUser) {
+		return userRepository.insert(newUser);
+	}
+	
+	public void delete(String id) {
+		findById(id);
+		userRepository.deleteById(id);
+	}
+	
+	public User update(User newUser) {
+		User oldUser = userRepository.findById(newUser.getId()).get();
+		User user = updateData(oldUser, newUser);
+		return userRepository.save(user);
+	}
+	
+	public User fromDTO(UserDTO userDataTransfer) {
+		return new User(userDataTransfer.getId(), userDataTransfer.getName(), userDataTransfer.getEmail());
+	}
+	
+	private User updateData(User user, User newUser) {
+		user.setName(newUser.getName());
+		user.setEmail(newUser.getEmail());
+		return user;
+	}
+	
 }
