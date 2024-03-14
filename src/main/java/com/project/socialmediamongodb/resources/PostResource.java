ec4c4ffb47d941f7b5c9440a037e7c1559ad5a67
Gustavo Correia <gustavoCorreiaa0@gmail.com>
2024-03-14 20:11:51 -0300

    Adding post services

---
+++ src/main/java/com/project/socialmediamongodb/resources/PostResource.java
@@ -0,0 +1,33 @@
+package com.project.socialmediamongodb.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.project.socialmediamongodb.domain.Post;
+import com.project.socialmediamongodb.services.PostService;
+
+@RestController
+@RequestMapping(value="/posts")
+public class PostResource {
+	
+	@Autowired
+	private PostService postService;
+
+	@GetMapping
+	public ResponseEntity<List<Post>> findAll(){
+		return ResponseEntity.ok().body(postService.findAll());
+	}
+	
+	@GetMapping(value="/{id}")
+	public ResponseEntity<Post> findById(@PathVariable String id){
+		return ResponseEntity.ok().body(postService.findById(id));
+		
+	}
+	
+}
---
+++ src/main/java/com/project/socialmediamongodb/services/PostService.java
@@ -0,0 +1,25 @@
+package com.project.socialmediamongodb.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.project.socialmediamongodb.domain.Post;
+import com.project.socialmediamongodb.repositories.PostRepository;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository postRepository;
+
+	public List<Post> findAll(){
+		return postRepository.findAll();
+	}
+	
+	public Post findById(String id) {
+		return postRepository.findById(id).get();
+	}
+	
+}
